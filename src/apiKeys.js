4204365314c8b2b4e5a1be81bf48dd75632f80cd
Pratiksha <istamshetti@gmail.com>
2023-10-06 02:15:36 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7725663bafa6d94a9e7a3df29d418711",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
